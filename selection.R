66f8a16c135b00e2c78b5a0e059ee516be250722
sophiekeller <skeller2022@gmail.com>
2021-05-02 15:34:32 -0400

    worked on regressions

--- selection.R
+++ selection.R
@@ -9,44 +9,31 @@
 
 source("dataBuild.R")
 load("covidCorruption.RData")
-regfit_full = regsubsets(gdp50~total_cases + total_deaths + population + stringency_index + human_development_index , data = masterData)
-regfit_full = regsubsets(gdp50~total_cases + total_deaths + population + stringency_index + human_development_index + corruptionRank + govRank + stabilityRank + regulationRank + lawRank + accountRank, data = masterData)
+masterData = masterData[masterData$date == "2020-03-19"]
+regfit_full = regsubsets(deltaGDP~total_cases + total_deaths + corruptionRank + govRank + stabilityRank + regulationRank + lawRank + accountRank, data = masterData)
 
 
 summary(regfit_full)
 reg_summary = summary(regfit_full)
-names(reg_summary)
 
 res.sum <- summary(regfit_full)
+
 data.frame(
   Adj.R2 = which.max(res.sum$adjr2),
   CP = which.min(res.sum$cp),
   BIC = which.min(res.sum$bic)
 )
 
-get_cv_error <- function(model.formula, data){
-  set.seed(1)
-  train.control <- trainControl(method = "cv", number = 5)
-  cv <- train(model.formula, data = data, method = "lda",
-              trControl = train.control)
-  cv$results$RMSE
-}
-get_model_formula <- function(id, object, outcome){
-  # get models data
-  models <- summary(object)$which[id,-1]
-  # Get outcome variable
-  #form <- as.formula(object$call[[2]])
-  #outcome <- all.vars(form)[1]
-  # Get model predictors
-  predictors <- names(which(models == TRUE))
-  predictors <- paste(predictors, collapse = "+")
-  # Build model formula
-  as.formula(paste0(outcome, "~", predictors))
-}
+regfit_full = regsubsets(deltaGDP~total_cases + total_deaths + corruptionRank + govRank + stabilityRank + regulationRank + lawRank + accountRank, data = masterData)
 
-model.ids <- 1:5
-cv.errors <-  map(model.ids, get_model_formula, regfit_full, "gdp50") %>%
-  map(get_cv_error, data = swiss) %>%
-  unlist()
-cv.errors
 
+summary(regfit_full)
+reg_summary = summary(regfit_full)
+
+res.sum <- summary(regfit_full)
+
+data.frame(
+  Adj.R2 = which.max(res.sum$adjr2),
+  CP = which.min(res.sum$cp),
+  BIC = which.min(res.sum$bic)
+)
